b370203ce9fce76dd291daddbf4b2943f984191e
eda3 <gerorin1010@gmail.com>
2020-08-26 07:35:39 +0900

    b

--- abc175/src/bin/b.rs
+++ abc175/src/bin/b.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
 fn main() {
-    unimplemented!();
+ proconio::input!{l:[u64]};
+ println!("{:?}",l.into_iter().sorted().tuple_combinations().filter(|&(a,b,c)|a+b>c && a!=b && b!=c).count());
 }
