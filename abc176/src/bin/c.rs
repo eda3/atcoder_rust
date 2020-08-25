a91bd81ca642c6fc93016359398d9ebfb5b02143
eda3 <gerorin1010@gmail.com>
2020-08-25 23:16:48 +0900

    c

--- abc176/src/bin/c.rs
+++ abc176/src/bin/c.rs
@@ -1,3 +1,10 @@
-fn main() {
-    unimplemented!();
+fn main(){
+ proconio::input!{n:u64,a:[u64;n]}
+ let mut m=0;
+ let mut x=0;
+ for &i in a.iter(){
+  if i<m{x+=m-i;}
+  m=m.max(i);
+ }
+ println!("{}",x);
 }
