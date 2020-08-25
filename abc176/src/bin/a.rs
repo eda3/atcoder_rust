8ab15563b239f342fa051ea1164f81162ddf1731
eda3 <gerorin1010@gmail.com>
2020-08-25 19:57:10 +0900

    a

---
+++ abc176/src/bin/a.rs
@@ -0,0 +1,4 @@
+fn main() {
+ proconio::input!{n:u64,x:u64,t:u64}
+ println!("{}", (n+x-1)/x*t);
+}
---
+++ abc176/src/bin/b.rs
@@ -0,0 +1,4 @@
+fn main() {
+    proconio::input!{n:u64}
+    println!(if n%9==0{"Yes"}else{"No"});
+}
