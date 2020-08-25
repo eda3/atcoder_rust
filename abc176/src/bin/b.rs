cc9ff5d5bab62a1074e22ca6db6d7fcf1f1b133f
eda3 <gerorin1010@gmail.com>
2020-08-25 22:58:53 +0900

    b

--- abc176/src/bin/b.rs
+++ abc176/src/bin/b.rs
@@ -1,4 +1,4 @@
-fn main() {
-    proconio::input!{n:u64}
-    println!(if n%9==0{"Yes"}else{"No"});
+fn main(){
+ proconio::input!{n:String}
+ println!("{}",if n.chars().map(|x|x.to_digit(10).unwrap()).sum::<u32>()%9==0{"Yes"}else{"No"});
 }
