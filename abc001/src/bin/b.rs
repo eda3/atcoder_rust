9d924b6e04f90c3c1fbcf8f1b621a69865ebaef7
eda3 <gerorin1010@gmail.com>
2020-08-25 18:53:04 +0900

    b

--- abc001/src/bin/b.rs
+++ abc001/src/bin/b.rs
@@ -1,3 +1,5 @@
-fn main() {
-    unimplemented!();
+use proconio::*;
+fn main(){
+    input!{m:isize}
+    println!("{:02}",if m<100{0}else if m<=5000{m/100}else if m<=30000{m/1000+50}else if m<=70000{((m/1000)-30)/5+80}else{89});
 }
