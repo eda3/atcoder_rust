71f5a44095b84d2cdde8205f4e590a5c1cb12f29
eda3 <gerorin1010@gmail.com>
2020-08-26 23:16:25 +0900

    c

--- abc175/src/bin/c.rs
+++ abc175/src/bin/c.rs
@@ -1,3 +1,12 @@
 fn main() {
-    unimplemented!();
+    proconio::input!{mut x:i64,mut k:i64,d:i64};
+    println!("x={} k={} d={}", x,k,d);
+    // 移動回数と、x/d(初期位置から、移動距離を割った値)を比較
+    let s=k.min(x/d);
+    println!("s={:?}", s);
+    k-=s;
+    println!("k={:?}", k);
+    x -= s*d;
+    let ans = if k%2==0{x}else{d-x};
+    println!("ans{:?}", ans);
 }
