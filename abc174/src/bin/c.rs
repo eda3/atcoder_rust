2476818abe4e85a4135f72f62567527ca2908bef
eda3 <gerorin1010@gmail.com>
2020-08-26 23:56:56 +0900

    c

--- abc174/src/bin/c.rs
+++ abc174/src/bin/c.rs
@@ -1,3 +1,12 @@
-fn main() {
-    proconio::input!{k:u64};
+fn main(){
+ proconio::input!{k:u64};
+ let mut x=0;
+ for i in 1..=k{
+  x=(x*10+7)%k;
+  if x ==0{
+   println!("{}",i);
+   return;
+  }
+ }
+ println!("-1");
 }
