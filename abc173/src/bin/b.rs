1a94a96f993fc8061c69ab5aa8c9cfb8604b70b8
eda3 <gerorin1010@gmail.com>
2020-08-28 06:34:09 +0900

    b

--- abc173/src/bin/b.rs
+++ abc173/src/bin/b.rs
@@ -1,3 +1,10 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{s:[String]};
+  let mut m=std::collections::HashMap::new();
+  for i in s{
+    *m.entry(i).or_insert(0)+=1;
+  }
+  for s in &["AC","WA","TLE","RE"]{
+    println!("{} x {}", s,m.get(&s.to_string()).unwrap_or(&0));
+  }
 }
