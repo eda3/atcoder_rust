1a9e8495c31dad526f1e969ed7112e957d0c8baa
eda3 <gerorin1010@gmail.com>
2020-08-30 22:35:19 +0900

    no

--- abc173/src/bin/c.rs
+++ abc173/src/bin/c.rs
@@ -1,3 +1,21 @@
+use proconio::{input, marker::Bytes};
 fn main() {
-    unimplemented!();
+  input!(h:usize, w:usize, k:usize, c:[Bytes; h]);
+  println!("h={:?}", h);
+  println!("1<<h={:?}", 1<<h);
+  println!("{:?}", w);
+  println!("{:?}", k);
+  println!("{:?}", c);
+
+  let mut r = 0;
+  for a in 0..1<<h{
+    for b in 0..1<<w{
+      let mut v=0;
+      println!("a={:?}", a);
+      println!("b={:?}", b);
+      println!()
+
+    }
+
+  };
 }
