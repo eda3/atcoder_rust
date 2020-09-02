aac931fae5e11c22f1a8dc3f6ac367700edf7580
eda3 <gerorin1010@gmail.com>
2020-09-03 05:37:06 +0900

    c

--- abc170/src/bin/c.rs
+++ abc170/src/bin/c.rs
@@ -1,3 +1,13 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{x:i32,n:usize,p:[i32;n]};
+  let mut ans = -200;
+  for i in 0..101{
+    if p.contains(&i){
+      continue
+    }
+    if (i-x).abs()<(ans-x).abs(){
+      ans = i
+    }
+  }
+  println!("{}",ans)
 }
