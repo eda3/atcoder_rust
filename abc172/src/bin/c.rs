513926673b841e5bd34b16c4978d5671cf7a6901
eda3 <gerorin1010@gmail.com>
2020-09-02 22:32:34 +0900

    c

--- abc172/src/bin/c.rs
+++ abc172/src/bin/c.rs
@@ -1,3 +1,16 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{n:usize,mut m:usize,k:usize,mut a:[usize;n],mut b:[usize;m]};
+  a.insert(0,0);
+  let mut t=b.iter().sum::<usize>();
+  let mut x=0;
+  for i in 0..a.len(){
+    t+=a[i];
+    while t>k&&m>0{
+      m-=1;
+      t-=b[m]
+    }
+    if t>k{break}
+    x=x.max(m+i)
+  }
+  println!("{}",x)
 }
