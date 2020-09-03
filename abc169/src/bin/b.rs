f658ca0a719be171aefcc6bbf732db6b7ebb02be
eda3 <gerorin1010@gmail.com>
2020-09-03 21:07:03 +0900

    b

--- abc169/src/bin/b.rs
+++ abc169/src/bin/b.rs
@@ -1,3 +1,13 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{n:u64,mut a:[usize;n]};
+  let mut ans = 1;
+  a.sort();
+  for i in a{
+    ans*=i;
+    if(10 as usize).pow(18)<ans{
+      println!("{}",-1);
+      return
+    }
+  }
+  println!("{}",ans)
 }
