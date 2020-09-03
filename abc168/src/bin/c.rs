1dd5aaa87c3126cb240ff2929616b8e910204e23
eda3 <gerorin1010@gmail.com>
2020-09-03 23:33:26 +0900

    c

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -1,3 +1,7 @@
+use std::f64::consts::PI;
 fn main() {
-    unimplemented!();
+  proconio::input!{a:f64,b:f64,h:f64,m:f64};
+  let mut c=(h*30.+m*0.5).abs()-(m*6.);
+  if 180.<=c{c=360.-c}
+  println!("{}",(a*a+b*b-2.*a*b*c.to_radians().cos()).sqrt());
 }
