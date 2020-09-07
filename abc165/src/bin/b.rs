cfa9d7ffd9cecabc54bbd83c3dfb05bbb7204d77
eda3 <gerorin1010@gmail.com>
2020-09-07 21:57:29 +0900

    b

--- abc165/src/bin/b.rs
+++ abc165/src/bin/b.rs
@@ -1,3 +1,10 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{mut x:usize};
+  let mut i = 0;
+  let mut c = 100;
+  while c < x{
+    i += 1;
+    c = c + c/100;
+  }
+  println!("{}", i)
 }
