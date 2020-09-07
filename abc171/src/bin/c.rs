8cb93c92274ac003085cfa5abe2c6455c3b2286f
eda3 <gerorin1010@gmail.com>
2020-09-07 19:14:32 +0900

    c

--- abc171/src/bin/c.rs
+++ abc171/src/bin/c.rs
@@ -1,10 +1,10 @@
-fn main() {
+fn main(){
   proconio::input!{mut n:u64};
   let mut s=String::new();
   while n>0{
-    n -= 1;
+    n-=1;
     s.insert(0,((n%26)as u8+b'a') as char);
-    n /=26;
+    n/=26
   }
-  println!("{}",s);
+  println!("{}",s)
 }
