9cddf59fa5ab390d94999ce79957c7716e28e9b2
eda3 <gerorin1010@gmail.com>
2020-09-07 22:23:46 +0900

    b

--- abc164/src/bin/b.rs
+++ abc164/src/bin/b.rs
@@ -1,3 +1,14 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{mut a:u64,b:u64,mut c:u64,d:u64};
+  let mut f = true;
+  while 0 < a && 0 < c{
+    if f{
+      c -= b;
+      f = false
+    }else{
+      a -= d;
+      f = true
+    }
+  }
+  println!("{}", if a==0{"No"}else{"Yes"})
 }
