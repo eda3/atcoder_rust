d9a9f9efcd2f382bfb13ba2e86d1221d2986cc05
eda3 <gerorin1010@gmail.com>
2020-09-07 20:41:13 +0900

    b

--- abc166/src/bin/b.rs
+++ abc166/src/bin/b.rs
@@ -1,3 +1,16 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{n:usize,k:usize};
+  let mut has = vec![true;n];
+  for _ in 0..k{
+    proconio::input!{d:usize,a:[usize;d]};
+    println!("d={:?}:a={:?}", d, a);
+    for j in 0..d{
+      println!("j={:?}", j);
+      println!("a[j]-0={:?}", a[j]-1);
+      has[a[j]-1] = false;
+    }
+    println!()
+  }
+  println!("{:?}", has.iter());
+  println!("{:?}", has.iter().filter(|x|**x).count())
 }
