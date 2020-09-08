8413aa666ca6e37b7116197ad522f3e99659d9e3
eda3 <gerorin1010@gmail.com>
2020-09-08 22:41:32 +0900

    c

--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -1,3 +1,11 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{n:usize,a:[usize;n-1]};
+  let mut x:Vec<usize> = vec![0;n];
+  // println!("{:?}",n);
+  // println!("{:?}",a);
+  // println!("{:?}",x);
+  for i in a.iter(){
+    x[i-1] += 1;
+  }
+  x.iter().for_each(|x|println!("{}",x))
 }
