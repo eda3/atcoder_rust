4f40b5977fda9d7d76d49e5b39d4e1a7bf96517c
eda3 <gerorin1010@gmail.com>
2020-09-09 23:11:20 +0900

    c

--- abc162/src/bin/c.rs
+++ abc162/src/bin/c.rs
@@ -1,3 +1,6 @@
+use num_integer::gcd;
+use itertools::iproduct;
 fn main() {
-    unimplemented!();
+  proconio::input!{n:i32};
+  println!("{}",iproduct!(1..=n,1..=n,1..=n).fold(0,|sum,x|sum+gcd(x.0,gcd(x.1,x.2))))
 }
