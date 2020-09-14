d1af1e2e3b20462a68ec2965a5dfb125d92a5aff
eda3 <gerorin1010@gmail.com>
2020-09-14 23:03:18 +0900

    c

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -3,5 +3,5 @@ fn main() {
   proconio::input!{a:f64,b:f64,h:f64,m:f64};
   let mut c=(h*30.+m*0.5).abs()-(m*6.);
   if 180.<=c{c=360.-c}
-  println!("{}",(a*a+b*b-2.*a*b*c.to_radians().cos()).sqrt());
+  println!("{}",(a*a+b*b-2.*a*b*c.to_radians().cos()).sqrt())
 }
