7515b8da919eff67916a4f24e598fe1a68828fc3
eda3 <gerorin1010@gmail.com>
2020-10-06 17:21:44 +0900

    a

--- abc167/src/bin/b.rs
+++ abc167/src/bin/b.rs
@@ -1,5 +1,4 @@
-use itertools::Itertools;
-fn main() {
-  proconio::input!{a:u64,b:u64,c:u64,k:u64};
-  println!("{}",a.min(k)-0.max(k-a-b))
+fn main(){
+  proconio::input!{a:i64,b:i64,_:i64,k:i64};
+  println!("{}",a.min(k)-0.max(k-a-b));
 }
