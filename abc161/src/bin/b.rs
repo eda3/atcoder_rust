7325715fc496ebb3b2819d5821a979c64ae0f1eb
eda3 <gerorin1010@gmail.com>
2020-10-06 18:48:28 +0900

    commit

--- abc161/src/bin/b.rs
+++ abc161/src/bin/b.rs
@@ -1,3 +1,14 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{n:usize,m:usize,a:[isize;n]};
+  println!("{:?}", n); // 4 n種類の賞品
+  println!("{:?}", m); // 1 m個を選ぶ
+  println!("{:?}", a); // [5, 4, 2, 1] 各賞品の投票数
+
+  let total = a.iter().sum();
+  println!("{}", total); // 12 合計投票数
+
+  let count = a.iter().filter(|x|4 * (m as isize) * **x >= total).count();
+  println!("{}", count); // 2
+
+  println!("{}",if count>=m{"Yes"}else{"No"}) // Yes
 }
