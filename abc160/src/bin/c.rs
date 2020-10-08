3121854db5de32e23c17812dee05ee98af233bec
eda3 <gerorin1010@gmail.com>
2020-10-09 06:21:49 +0900

    c

--- abc160/src/bin/c.rs
+++ abc160/src/bin/c.rs
@@ -1,3 +1,14 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{k:u64,n:usize,a:[u64;n]};
+  println!("{:?}",k);
+  println!("{:?}",n);
+  println!("{:?}",a);
+
+  // 湖を囲う家のうち、家と家の距離が一番長い地点を特定
+  let mut c = a[0] + k - a[n-1];
+  for i in 0..n-1{
+    c = c.max(a[i+1] - a[i]);
+    println!("{}",c);
+  }
+  println!("{:?}",k-c);
 }
