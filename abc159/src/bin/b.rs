a559c11735d69adb7d8f9aa41503c717deecd237
eda3 <gerorin1010@gmail.com>
2020-10-14 19:43:19 +0900

    b

--- abc159/src/bin/b.rs
+++ abc159/src/bin/b.rs
@@ -1,3 +1,9 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{s:String};
+  let l=s.len(); // 文字列の長さ
+  let c=l/2; // 中心
+  // println!("{}",&s[0..c]); // 先頭から中心まで
+  // println!("{}",s[c+1..l]) // 中心から最後尾まで
+  println!("{}",if &s[0..c]==&s[c+1..l]{"Yes"}else{"No"})
+
 }
