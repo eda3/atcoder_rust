658cae2ba400892c24139de38ae970b2cb367547
eda3 <gerorin1010@gmail.com>
2020-11-15 14:50:05 +0900

    c

--- abc163/src/bin/c.rs
+++ abc163/src/bin/c.rs
@@ -1,9 +1,6 @@
-fn main() {
+fn main(){
   proconio::input!{n:usize,a:[usize;n-1]};
   let mut x:Vec<usize> = vec![0;n];
-  // println!("{:?}",n);
-  // println!("{:?}",a);
-  // println!("{:?}",x);
   for i in a.iter(){
     x[i-1] += 1;
   }
