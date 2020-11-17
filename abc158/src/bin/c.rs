c15b66e1b3cd13ac304f97f7f33fe2ea8324c99c
eda3 <gerorin1010@gmail.com>
2020-11-17 18:56:13 +0900

    c

--- abc158/src/bin/c.rs
+++ abc158/src/bin/c.rs
@@ -1,3 +1,9 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{a:i32,b:i32};
+  println!("{}",
+    (0..1010)
+      .filter(|x| x * 8 / 100 == a && x * 10 / 100 == b)
+      .nth(0)
+      .unwrap_or(-1)
+  )
 }
