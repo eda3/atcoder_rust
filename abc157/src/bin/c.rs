0d6bb3e345b8cb0cc0f8949949d13c626ef7ed96
eda3 <gerorin1010@gmail.com>
2020-12-01 19:47:33 +0900

    c

--- abc157/src/bin/c.rs
+++ abc157/src/bin/c.rs
@@ -1,3 +1,18 @@
-fn main() {
-    unimplemented!();
+fn main(){
+  proconio::input!{n:u32,m:usize,ss:[(usize, char);m]};
+  println!("n{:?}",n);
+  println!("sc{:?}",ss);
+  let ans = (0..10i32.pow(n))
+    .filter(|x| {
+      let x = x.to_string().as_bytes().to_vec();
+      x.len() == n as usize
+      && ss
+        .iter()
+        .all(|(s,c)| matches!(x.get(*s-1), Some(x) if *x as char == *c))
+
+    })
+    .next()
+    .unwrap_or(-1);
+  dbg!(ans);
+  println!("{:?}",ans);
 }
