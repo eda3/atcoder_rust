5a4b722e46799aff57c9391af3999c4a1a1a1c47
eda3 <gerorin1010@gmail.com>
2021-01-23 07:51:58 +0900

    b

--- abc156/src/bin/b.rs
+++ abc156/src/bin/b.rs
@@ -1,3 +1,9 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{mut n:u64,k:u64};
+  let mut a = 0;
+  while n > 0{
+    n /= k;
+    a += 1
+  }
+  println!("{}",a);
 }
--- abc157/src/bin/c.rs
+++ abc157/src/bin/c.rs
@@ -1,7 +1,5 @@
 fn main(){
   proconio::input!{n:u32,m:usize,ss:[(usize, char);m]};
-  println!("n{:?}",n);
-  println!("sc{:?}",ss);
   let ans = (0..10i32.pow(n))
     .filter(|x| {
       let x = x.to_string().as_bytes().to_vec();
@@ -13,6 +11,5 @@ fn main(){
     })
     .next()
     .unwrap_or(-1);
-  dbg!(ans);
-  println!("{:?}",ans);
+  println!("{}",ans);
 }
