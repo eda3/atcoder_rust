45585c1f2954a2f00b2434af5d8705ee618043b1
eda3 <gerorin1010@gmail.com>
2021-02-10 06:22:41 +0900

    b

--- abc161/src/bin/b.rs
+++ abc161/src/bin/b.rs
@@ -1,14 +1,15 @@
 fn main(){
   proconio::input!{n:usize,m:usize,a:[isize;n]};
-  println!("{:?}", n); // 4 n種類の賞品
-  println!("{:?}", m); // 1 m個を選ぶ
-  println!("{:?}", a); // [5, 4, 2, 1] 各賞品の投票数
+  // println!("{:?}", n); // 4 n種類の賞品
+  // println!("{:?}", m); // 1 m個を選ぶ
+  // println!("{:?}", a); // [5, 4, 2, 1] 各賞品の投票数
 
   let total = a.iter().sum();
-  println!("{}", total); // 12 合計投票数
+  // println!("{}", total); // 12 合計投票数
 
+  // let count = a.iter().filter(|x|4 * (m as isize) * **x >= total).count();
   let count = a.iter().filter(|x|4 * (m as isize) * **x >= total).count();
-  println!("{}", count); // 2
+  // println!("{:?}", count); // 2
 
-  println!("{}",if count>=m{"Yes"}else{"No"}) // Yes
+  println!("{}",if count>=m{"Yes"}else{"No"})
 }
