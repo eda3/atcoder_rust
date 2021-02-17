1ac65cbf8600f815906d83a1d1be29e83b0c9174
eda3 <gerorin1010@gmail.com>
2021-02-18 06:49:05 +0900

    c

--- abc156/src/bin/c.rs
+++ abc156/src/bin/c.rs
@@ -1,3 +1,16 @@
 fn main() {
-    unimplemented!();
+  proconio::input!{m:i32,a:[i32;m]};
+  let &min = a.iter().min().unwrap();
+  let &max = a.iter().max().unwrap();
+  let mut min_power = std::i32::MAX;
+  for p in min..max {
+    let power =
+      a.iter()
+        .map(|n|(n-p)*(n-p))
+        .fold(0,|sum,val|sum+val);
+    if power < min_power {
+      min_power = power;
+    }
+  }
+  println!("{}",min_power);
 }
