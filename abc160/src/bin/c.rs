e9a8457bb7352ec03245f4f36196a807ce7ba003
eda3 <gerorin1010@gmail.com>
2021-03-05 20:23:53 +0900

    c

--- abc160/src/bin/c.rs
+++ abc160/src/bin/c.rs
@@ -1,14 +1,9 @@
-fn main(){
-  proconio::input!{k:u64,n:usize,a:[u64;n]};
-  println!("{:?}",k);
-  println!("{:?}",n);
-  println!("{:?}",a);
+fn main() {
+  proconio::input! {k:u64,n:usize,a:[u64;n]};
 
-  // 湖を囲う家のうち、家と家の距離が一番長い地点を特定
-  let mut c = a[0] + k - a[n-1];
-  for i in 0..n-1{
-    c = c.max(a[i+1] - a[i]);
-    println!("{}",c);
+  let mut c = a[0] + k - a[n - 1];
+  for i in 0..n - 1 {
+    c = c.max(a[i + 1] - a[i]);
   }
-  println!("{}",k-c)
+  println!("{}", k-c)
 }
